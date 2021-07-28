b654eb3950c3143fe1b3f606e5f2652a420e90f2
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-07-28 08:16:17 +0530

    Create Kth smallest element.cpp

---
+++ Kth smallest element.cpp
@@ -0,0 +1,14 @@
+int kthSmallest(int arr[], int l, int r, int k) {
+        
+        int n=r-l+1;
+        priority_queue<int>pq;
+        
+        for(int i=0;i<n;i++){
+            pq.push(arr[i]);
+            
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
